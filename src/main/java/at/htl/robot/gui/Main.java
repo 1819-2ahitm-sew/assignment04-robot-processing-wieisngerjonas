cb38023e63285dceff08bbb8123624b3d3dbad4d
ThomasStuetz <thomas.stuetz@gmx.at>
2018-09-30 09:55:27 +0200

    add the instructions and the starter project

---
+++ src/main/java/at/htl/robot/gui/Main.java
@@ -0,0 +1,73 @@
+package at.htl.robot.gui;
+
+import at.htl.robot.model.Robot;
+import processing.core.PApplet;
+
+
+public class Main extends PApplet {
+
+    // Hier die Member-Attribute eintragen
+
+    public static void main(String[] args) {
+        PApplet.main("at.htl.robot.gui.Main", args);
+    }
+
+    public void settings() {
+        size(800, 800);
+    }
+
+    public void setup() {
+        background(209); //https://processing.org/tutorials/color/
+
+
+    }
+
+    /**
+     * Diese Methode wird iterativ durchlaufen (wie loop() beim Arduino)
+     */
+    public void draw() {
+
+    }
+
+    /**
+     * Erstellen Sie eine eigene Methode, mittels der der Roboter am Bildschirm gezeichnet wird
+     * Die Angabe zu Position des Roboters am Spielfeld erhalten Sie aus dem Roboter-Objekt, welches
+     * als Parameter übergeben wird.
+     *
+     * @param robot Objekt des zu zeichnenden Roboters
+     */
+    public void drawRobot(Robot robot) {
+
+
+    }
+
+    /**
+     * Erstellen Sie eine eigene Methode zum Löschen des Bildschirms
+     */
+    public void deleteAll() {
+
+    }
+
+    /**
+     * In dieser Methode reagieren Sie auf die Tasten
+     */
+    public void keyPressed() {
+        println("pressed " + key + " " + keyCode);
+
+        if (key == 'f' || key == 'F') {
+
+        } else if (key == 'l' || key == 'L') {
+
+        }
+
+    }
+
+//    public void keyTyped() {
+//        println("typed " + key + " " + keyCode);
+//    }
+//
+//    public void keyReleased() {
+//        println("released " + key + " " + keyCode);
+//    }
+
+}
